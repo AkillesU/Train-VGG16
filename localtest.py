9c61287e2765485f420025e5ce8e40a2309babba
AkillesU <a.rechardt@gmail.com>
2023-02-11 11:54:15 +0000

    Deleted test file and created 1 epoch run of training file

--- localtest.py
+++ localtest.py
@@ -2,27 +2,23 @@ import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-
 import keras.losses
 import tensorflow as tf
 
 import wandb
 from wandb.keras import WandbCallback
 
-#Checking GPU compatibility
 gpus = tf.config.list_physical_devices('GPU')
 print(gpus)
+#wandb.init(project="Train-VGG16", entity="a-rechardt")
 
-#Initializing wandb
-wandb.init(project="Train-VGG16", entity="a-rechardt")
-
-epochs = 1
+epochs = 20
 batch_size = 32
 learning_rate = 0.001
 
 #Creating training dataset from fast-22 imagenet directory, defining batch size and prerpocessing image size
 train_ds = tf.keras.utils.image_dataset_from_directory(
-    "/fast-data22/datasets/ILSVRC/2012/clsloc/train",
+    "C:/Users/arech/Documents/Imagenet/imagenet-a",
     labels = 'inferred',
     label_mode= "int",
     color_mode="rgb",
@@ -35,7 +31,7 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
 )
 #Creating validation dataset from fast-22 imagenet directory, defining batch size and prerpocessing image size
 validation_ds = tf.keras.utils.image_dataset_from_directory(
-    "/fast-data22/datasets/ILSVRC/2012/clsloc/train",
+    "C:/Users/arech/Documents/Imagenet/imagenet-a",
     labels='inferred',
     label_mode="int",
     color_mode="rgb",
@@ -69,6 +65,3 @@ model.compile(
 model.fit(train_ds, epochs= epochs, verbose=1, validation_data=validation_ds, callbacks=[WandbCallback()])
 
 model.save_weights('trained_weights_VGG16/')
-
-
-
