934e385c9579788b5f0722006bb7faccff847b42
AkillesU <a.rechardt@gmail.com>
2023-02-11 14:05:46 +0000

    Working! Changed batch size from 32 to 256 and increased learning rate from 0.001 to 0.007 to optimize training performance.

--- pipeline.py
+++ pipeline.py
@@ -17,8 +17,8 @@ print(gpus)
 wandb.init(project="Train-VGG16", entity="a-rechardt")
 
 epochs = 1
-batch_size = 32
-learning_rate = 0.001
+batch_size = 256
+learning_rate = 0.007
 
 #Creating training dataset from fast-22 imagenet directory, defining batch size and prerpocessing image size
 train_ds = tf.keras.utils.image_dataset_from_directory(
