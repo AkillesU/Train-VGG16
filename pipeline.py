2268e3a95522cded15e19232dcec2232073aee33
AkillesU <a.rechardt@gmail.com>
2023-02-12 13:37:44 +0000

    Added imagenet weights and dropped learning rate to 0.0001

--- pipeline.py
+++ pipeline.py
@@ -13,12 +13,15 @@ from wandb.keras import WandbCallback
 gpus = tf.config.list_physical_devices('GPU')
 print(gpus)
 
-#Initializing wandb
-wandb.init(project="Train-VGG16", entity="a-rechardt")
 
-epochs = 1
+epochs = 10
 batch_size = 32
-learning_rate = 0.001
+learning_rate = 0.0001
+
+#Initializing wandb
+wandb.init(project="Train-VGG16", entity="a-rechardt", config={"epochs":epochs, "batch_size":batch_size, "learning_rate":learning_rate})
+
+
 
 #Creating training dataset from fast-22 imagenet directory, defining batch size and prerpocessing image size
 train_ds = tf.keras.utils.image_dataset_from_directory(
@@ -50,26 +53,21 @@ validation_ds = tf.keras.utils.image_dataset_from_directory(
 for images, labels in train_ds.take(1):
     print(images.shape)
     print(labels.shape)
-#Setting hyperparameters to wandb
-wandb.config = {
-  "learning_rate": learning_rate,
-  "epochs": epochs,
-  "batch_size": batch_size
-}
+
 #Creating model from keras library: pretrained vgg16 model
-model = tf.keras.applications.VGG16()
+model = tf.keras.applications.VGG16(weights='imagenet')
 
 #Setting model training hyperparameters
 model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate= learning_rate),
-    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    loss=keras.losses.SparseCategoricalCrossentropy(),
     metrics=["accuracy"]
 )
 #Training model and sending stats to wandb
 model.fit(train_ds, epochs= epochs, verbose=1, validation_data=validation_ds, callbacks=[WandbCallback()])
 
 model.save_weights('trained_weights_VGG16/')
-print("Done")
 
+exit("Done")
 
 
